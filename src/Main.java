ec8b821c57551d8cee11cdf99b404cf9a371c603
武海之 <123456>
2020-03-22 15:19:18 +0800

    fenzhi6666

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("asdfasdfasd");
-        System.out.println("1111111111111111111111");
-        System.out.println("1111111111111111111111");
-        System.out.println("1111111111111111111111");
-        System.out.println("1111111111111111111111");
-        System.out.println("1111111111111111111111");
-        System.out.println("1111111111111111111111");
+        System.out.println("9032-30-4324-30");
         System.out.println("Hello World!");
     }
 }
